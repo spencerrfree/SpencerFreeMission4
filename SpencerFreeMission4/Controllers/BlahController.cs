a9b9a576fd35193a18765b000897158e7907ca7a
spencerrfree <sf376@byu.edu>
2023-02-06 12:05:48 -0700

    Mission5 Branch

---
+++ SpencerFreeMission4/Controllers/BlahController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpencerFreeMission4.Controllers
+{
+    public class BlahController : Controller
+    {
+        public IActionResult Index()
+        {
+            
+            /*throw new IndexOutOfRangeException();*/
+            return View();
+        }
+    }
+}
